895c154df87d80477f2d8cec9a84b7f0adff44ed
Ananthoju-Pranav-Sai <ai20btech11004@iith.ac.in>
2023-11-21 12:29:07 +0530

    Cleaned causal discovery code

---
+++ Code/main.py
@@ -0,0 +1,89 @@
+from causallearn.search.ConstraintBased.PC import pc
+from causallearn.utils.cit import kci
+from dataset_generator import *
+import numpy as np
+from causallearn.utils.PCUtils.BackgroundKnowledge import BackgroundKnowledge
+from causallearn.graph.GraphClass import CausalGraph
+
+from sklearn.svm import SVR
+from hsic import hsic_gam
+from sklearn.model_selection import train_test_split
+
+
+def is_consistent(data, x, y):
+    X = data[:, x].reshape(-1, 1)
+    y = data[:, y]
+
+    # Fitting a non-linear regression
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    svr = SVR(kernel="rbf", gamma="auto")
+    svr.fit(X_train, y_train)
+
+    y_pred = svr.predict(X_test)
+    residuals = (y_test - y_pred).reshape(-1, 1)
+
+    test_stat, thresh = hsic_gam(X_test, residuals, 0.05)
+
+    return test_stat < 3*thresh
+
+# Uncomment the data generator which you would like to use and comment the others
+
+# Diamond
+DataGen = Diamond(
+    lambda: np.random.uniform(-3, 3),
+    lambda: np.random.normal(0, 1),
+    lambda: np.random.normal(0, 1),
+    lambda: np.random.normal(0, 1),
+)
+
+data = DataGen.generate_diamond(5000)
+
+# # Lin4v
+# DataGen = Lin4V(
+#     lambda: np.random.uniform(-5, 5),
+#     lambda: np.random.normal(0, 1),
+#     lambda: np.random.normal(0, 1),
+#     lambda: np.random.normal(0, 1),
+# )
+
+# data = DataGen.generate_lin4v(5000)
+
+# Semi-Synthetic German Credit
+# DataGen = GermanCredit(
+#     lambda: np.random.binomial(n=1, p=0.5),
+#     lambda: np.random.gamma(10, 3.5),
+#     lambda: np.random.normal(0, 0.5),
+#     lambda: np.random.normal(0, 2),
+#     lambda: np.random.normal(0, 3),
+#     lambda: np.random.normal(0, 2),
+#     lambda: np.random.normal(0, 5),
+# )
+
+# data = DataGen.generate_german_credit(5000)
+
+cg = CausalGraph(len(data))
+nodes = cg.G.get_nodes()
+
+# add any background knowledge 
+# bk = BackgroundKnowledge().add_required_by_node(nodes[0], nodes[2]).add_required_by_node(nodes[2], nodes[0])
+
+cg_with_bk = pc(
+    data,
+    alpha=0.05,
+    indep_test="kci",
+    kernelZ="Gaussian",
+    # background_knowledge=bk
+)
+
+print("Causal Graph obtained after PC:\n", cg_with_bk.find_adj())
+
+cg = cg_with_bk.find_adj()
+causal_graph = []
+for x, y in cg:
+    if is_consistent(data, x, y):
+        causal_graph.append((x, y))
+
+print("Causal Graph consistent with data:\n", causal_graph)
--- Code/svr.py
+++ Code/svr.py
@@ -1,5 +1,5 @@
 import numpy as np
-from dataset_generator import Diamond
+from dataset_generator import *
 from sklearn.svm import SVR
 from hsic import hsic_gam
 from sklearn.model_selection import train_test_split
@@ -22,23 +22,26 @@ def is_consistent(data, x, y):
 
     test_stat, thresh = hsic_gam(X_test, residuals, 0.05)
 
-    return test_stat < 3 * thresh
+    return test_stat < 3*thresh
 
 
-DataGen = Diamond(
-    lambda: np.random.uniform(-5, 5),
-    lambda: np.random.normal(0, 1),
-    lambda: np.random.normal(0, 1),
-    lambda: np.random.normal(0, 1),
-)
+if __name__ == "__main__":
+    DataGen = GermanCredit(
+        lambda: np.random.binomial(n=1, p=0.5),
+        lambda: np.random.gamma(10, 3.5),
+        lambda: np.random.normal(0, 0.5),
+        lambda: np.random.normal(0, 2),
+        lambda: np.random.normal(0, 3),
+        lambda: np.random.normal(0, 2),
+        lambda: np.random.normal(0, 5),
+    )
 
-data = DataGen.generate_diamond(10000)
+    data = DataGen.generate_german_credit(5000)
 
-# Lets assume that we use pc algorithm to find the edges in causal graph
-cg = [(0, 1), (1, 0), (1, 3), (3, 1), (3, 2), (2, 3), (0, 2), (2, 0)]
-causal_graph = []
-for x, y in cg:
-    if is_consistent(data, x, y):
-        causal_graph.append((x, y))
+    cg = [(5, 0), (2, 1), (3, 1), (4, 1), (5, 1), (0, 2), (1, 2), (1, 3), (4, 3), (0, 4), (1, 4), (3, 4), (0, 6), (5, 6), (2, 0), (4, 0), (6, 0), (0, 5), (1, 5), (6, 5), (2, 5), (5, 2)]
+    causal_graph = []
+    for x, y in cg:
+        if is_consistent(data, x, y):
+            causal_graph.append((x, y))
 
-print(causal_graph)
+    print(causal_graph)
